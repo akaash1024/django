81b3934a5ce40d4ffd22cbafac19670e31f42d90
akashkewat99 <akashkewat99@gmail.com>
2025-02-14 18:33:19 +0530

    updated

--- REST-FRAMEWORK Demo/drfdemo/cbvApp/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import StudentModel
-
-class StudentSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = StudentModel
-        fields = '__all__'
-        # fields = ['id','name','score']
